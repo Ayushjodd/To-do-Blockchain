46d781fac8078c302d35e5b28190d56d4b8e191c
Ayushjodd <ayushkalonia2016@gmail.com>
2024-07-30 15:57:15 +0530

    fixed the Router to routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Wallet from "./pages/Wallet";
 import ViewAllTask from "./pages/ViewAllTask";
 import CreateTask from "./pages/CreateTask";
@@ -9,17 +9,15 @@ import ViewTask from "./pages/ViewTask";
 
 export const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Router>
-          <Route path="/" element={Wallet} />
-          <Route path="/view-all-tasks" element={ViewAllTask} />
-          <Route path="/create-task" element={CreateTask} />
-          <Route path="/deleteTask" element={DeleteTask} />
-          <Route path="update-task" element={UpdateTask} />
-          <Route path="view-task" element={ViewTask} />
-        </Router>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Wallet />} />
+        <Route path="/view-all-tasks" element={<ViewAllTask />} />
+        <Route path="/create-task" element={<CreateTask />} />
+        <Route path="/delete-task" element={<DeleteTask />} />
+        <Route path="/update-task" element={<UpdateTask />} />
+        <Route path="/view-task" element={<ViewTask />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
