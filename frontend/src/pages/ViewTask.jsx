ac7347c7649643f562367687cf27c4e854a27293
Ayushjodd <ayushkalonia2016@gmail.com>
2024-08-01 12:55:37 +0530

    finished the view task component

--- frontend/src/pages/ViewTask.jsx
+++ frontend/src/pages/ViewTask.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function ViewTask() {
-  return <div>ViewTask</div>;
+  const [task, setTask] = useState([]);
+  const viewtask = async (e) => {
+    try {
+      e.preventDefault();
+      const taskID = document.querySelector("#taskID").value;
+      const res = await fetch(
+        `http://localhost:3000/api/ethereum/view-task/${taskID}`,
+        {
+          method: "GET",
+        }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={viewtask}>
+        <label>
+          ID:
+          <input id="taskID" />
+        </label>
+        <button type="submit">view task</button>
+      </form>
+    </>
+  );
 }
